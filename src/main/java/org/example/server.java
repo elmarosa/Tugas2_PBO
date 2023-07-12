d3c1f8ead08121e3f89bbdddec5976173dd6e9b0
elmarosa <elmayuliarosa@gmail.com>
2023-07-13 01:48:54 +0800

    6

--- src/main/java/org/example/server.java
+++ src/main/java/org/example/server.java
@@ -12,7 +12,30 @@ public class server {
         conn con = new conn();
     }
 
-    public void startServer() throws IOException {
+    public void startServer() throws IOException
+    {
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 8135),0);
+        httpServer.createContext( "/ecommerce", new ecommerceHandler());
+        httpServer.setExecutor(Executors.newSingleThreadExecutor());
+        httpServer.start();
+    }
+    public static class ecommerceHandler implements HttpHandler {
+        public void handle(HttpExchange exchange) throws IOException {
+            //post
+            //get
+
+            if("GET".equals(exchange.getRequestMethod())){
+                OutputStream outputStream = exchange.getResponseBody();
+                String tes = "Elmarosa";
+                exchange.sendResponseHeaders(200, tes.length());
+                outputStream.write(tes.getBytes());
+                outputStream.flush();
+                outputStream.close();
+            }
+        }
+    }
+
+    {
         HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 8135), 0);
         httpServer.createContext("/", new EcomHandler());
         httpServer.setExecutor(Executors.newSingleThreadExecutor());
